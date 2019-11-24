5b26cf6e69c283ac8a05b6d7f5c69ef8ace00d8b
Eric Berquist <eric.berquist@gmail.com>
2019-11-24 17:29:16 -0500

    [Julia] move code into package structure

--- julia/OverlapIntegrals/src/OverlapIntegrals.jl
+++ julia/OverlapIntegrals/src/OverlapIntegrals.jl
@@ -1,5 +1,59 @@
 module OverlapIntegrals
 
-greet() = print("Hello World!")
+export fact2, binomial_prefactor, overlap1d, dist2, product_center_1d, tho66
+
+function fact2(n)
+    if n <= 0
+        return 1
+    else
+        return n * fact2(n - 2)
+    end
+end
+
+function binomial_prefactor(s::Integer, ia::Integer, ib::Integer, xpa::Number, xpb::Number)
+    acc = 0
+    for t = 0 : s + 1
+        if (s - ia) <= t && t <= ib
+            acc += binomial(ia, s - t) * binomial(ib, t) * (xpa ^ (ia - s + t)) * (xpb ^ (ib - t))
+        end
+    end
+    return acc
+end
+
+
+function overlap1d(l1::Integer, l2::Integer, pax::Number, pbx::Number, gamma::Number)
+    acc = 0.0
+    for i = 0 : 1 + floor(0.5 * (l1 + l2))
+        acc += binomial_prefactor(Integer(2 * i), l1, l2, pax, pbx) * fact2(2 * i - 1) / ((2 * gamma) ^ i)
+    end
+    return acc
+end
+
+function dist2(xa, ya, za, xb, yb, zb)
+    return (xa - xb) * (xa - xb) + (ya - yb) * (ya - yb) + (za - zb) * (za - zb)
+end
+
+function product_center_1d(za, xa, zb, xb)
+    return ((za * xa) + (zb * xb)) / (za + zb)
+end
+
+function tho66(alpha1::Number, alpha2::Number, ra, rb, la, lb)
+    gamma = alpha1 + alpha2
+    xa, ya, za = ra
+    xb, yb, zb = rb
+    rab2 = dist2(xa, ya, za, xb, yb, zb)
+    l1, m1, n1 = la
+    l2, m2, n2 = lb
+    xp = product_center_1d(alpha1, xa, alpha2, xb)
+    yp = product_center_1d(alpha1, ya, alpha2, yb)
+    zp = product_center_1d(alpha1, za, alpha2, zb)
+
+    pre = exp(-alpha1 * alpha2 * rab2 / gamma) * (pi / gamma) ^ 1.5
+
+    wx = overlap1d(l1, l2, xp - xa, xp - xb, gamma)
+    wy = overlap1d(m1, m2, yp - ya, yp - yb, gamma)
+    wz = overlap1d(n1, n2, zp - za, zp - zb, gamma)
+    return pre * wx * wy * wz
+end
 
 end # module
--- julia/OverlapIntegrals/test/runtests.jl
+++ julia/OverlapIntegrals/test/runtests.jl
@@ -1,6 +1,43 @@
-using OverlapIntegrals
-using Test
+using OverlapIntegrals, Test
 
-@testset "OverlapIntegrals.jl" begin
-    # Write your own tests here.
+@testset "fact2" begin
+    @test fact2(0) == 1
+    @test fact2(1) == 1
+    @test fact2(2) == 2
+    @test fact2(3) == (3 * 1)
+    @test fact2(4) == (4 * 2)
+    @test fact2(5) == (5 * 3 * 1)
+    @test fact2(6) == (6 * 4 * 2)
+    @test fact2(7) == (7 * 5 * 3 * 1)
+end
+
+@testset "binomial_prefactor" begin
+    @test isapprox(binomial_prefactor(1, 1, 1, 0.1, 0.2), 0.3; atol = 1e-7)
+    @test isapprox(binomial_prefactor(1, 1, 1, 0.3, 0.4), 0.7; atol = 1e-7)
+    @test isapprox(binomial_prefactor(1, 3, 1, 0.1, 0.2), 0.007; atol = 1e-7)
+    @test isapprox(binomial_prefactor(2, 3, 1, 0.1, 0.2), 0.09; atol = 1e-7)
+end
+
+@testset "overlap1d" begin
+    @test isapprox(overlap1d(1, 1, 0.1, 0.2, 1.0), 0.52; atol = 1e-5)
+    @test isapprox(overlap1d(3, 1, 0.1, 0.2, 1.0), 0.7952; atol = 1e-5)
+end
+
+@testset "tho66" begin
+    za = 1.8
+    zb = 2.8
+    ra = [0.0, 0.0, 0.0]
+    rb = [0.5, 0.8, -0.2]
+
+    integral = tho66(za, zb, ra, rb, [0, 0, 0], [0, 0, 0])
+    @test isapprox(integral, 0.20373275913014607; atol = 1.0e-16)
+
+    integral = tho66(za, zb, ra, rb, [1, 0, 0], [0, 0, 0])
+    @test isapprox(integral, 0.062005622343957505; atol = 1.0e-16)
+
+    integral = tho66(za, zb, ra, rb, [1, 1, 0], [1, 1, 0])
+    @test isapprox(integral, -0.00043801221837779696; atol = 1.0e-16)
+
+    integral = tho66(za, zb, ra, rb, [2, 1, 0], [1, 1, 0])
+    @test isapprox(integral, -0.0002385994651113168; atol = 1.0e-16)
 end
