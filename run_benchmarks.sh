020a08714c8f5fac3e49ced33e0cad49640ab496
Eric Berquist <eric.berquist@gmail.com>
2020-12-17 23:56:45 -0500

    add language stubs to benchmarks runner

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -2,16 +2,60 @@
 
 set -v
 
+# C
+(
+    cd c
+)
+# Clojure
+(
+    cd clojure
+)
+# Common Lisp
+(
+    cd cl
+)
+# C++
+(
+    cd cpp
+)
+# Go
+(
+    cd go
+)
+# Hy
+(
+    cd hy
+)
 # Julia
 (
     cd julia
     cd OverlapIntegrals
     julia --project=. test/bench.jl
 )
+# Kotlin
+(
+    cd kotlin
+)
 # Nim
 (
     cd nim
     nim r -d:release --opt:speed bench.nim
+)
+# Python
+(
+    cd python
+)
+# Racket
+(
+    cd racket
+)
+# Ruby
+(
+    cd ruby
+)
+# Rust
+(
+    cd rust/overlapintegrals
 )
 
 set +v
